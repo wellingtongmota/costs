9c6a667142ad802655f909fcf302b8f1e171b9b5
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-03-10 15:48:28 -0300

    Configurando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+import Home from './routes/Home'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/routes/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App'
+
+const Home = () => {
+  return (
+    <div className="App">
+      <h1>Home</h1>
+      <p>Costs</p>
+    </div>
+  )
+}
+
+export default Home
