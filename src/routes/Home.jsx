40f1a8153f43f6f72dc7eb1ad4d9812a729e06e5
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-03-13 15:56:12 -0300

    Criacao layout LinkButton

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,14 @@
-const Home = () => {
-  return(
-    <div>
-      <p>Home</p>
-    </div>
+import styles from './Home.module.css'
+import LinkButton from '../layout/LinkButton'
+import savings from "../img/savings.svg"
+
+export default function Home() {
+  return (
+    <section className={styles.container_home}>
+      <h1>Bem-vindo ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <LinkButton to="/newproject" text="Criar projeto" />
+      <img src={savings} alt="Costs" />
+    </section>
   )
 }
-
-export default Home
